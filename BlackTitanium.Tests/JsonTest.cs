3d970e153e981e92f93a2a5e470cd8c43332a303
deltaDelete <samansy@yandex.ru>
2024-02-01 09:31:15 +0700

    wip

--- BlackTitanium.Tests/JsonTest.cs
+++ BlackTitanium.Tests/JsonTest.cs
@@ -1,4 +1,5 @@
 using BlackTitanium.Models;
+using JsonSubTypes;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using Xunit.Abstractions;
@@ -6,51 +7,54 @@ using Xunit.Abstractions;
 namespace BlackTitanium.Tests;
 
 public class JsonTest {
-    private readonly ITestOutputHelper output;
+    private readonly ITestOutputHelper _output;
+
+    private readonly JsonSerializerSettings _jsonSerializerSettings =
+        new JsonSerializerSettings {
+            Formatting = Formatting.Indented,
+            ContractResolver = new CamelCasePropertyNamesContractResolver(),
+        };
 
     private const string ExpectedJson = """
-                                               {
-                                                 "$type": "BlackTitanium.Models.Error, BlackTitanium",
-                                                 "message": "Test error",
-                                                 "code": -1
-                                               }
-                                               """;
+                                        {
+                                          "message": "Test error",
+                                          "code": -1,
+                                          "type": "error"
+                                        }
+                                        """;
 
     public JsonTest(ITestOutputHelper output) {
-        this.output = output;
+        this._output = output;
+        _jsonSerializerSettings.Converters.Add(
+            JsonSubtypesConverterBuilder
+                .Of<Response>("type")
+                .RegisterSubtype<Error>("error")
+                .RegisterSubtype<Ok>("ok")
+                .SerializeDiscriminatorProperty(true)
+                .Build()
+        );
+        _jsonSerializerSettings.TypeNameHandling = TypeNameHandling.Auto;
+        JsonConvert.DefaultSettings = () => _jsonSerializerSettings;
     }
 
     [Fact]
     public void Derived_type_serialization() {
-        var error = new Models.Error() {
+        Response error = new Error() {
             Code = -1,
             Message = "Test error"
         };
-        var jsonSerializerSettings = new JsonSerializerSettings {
-            TypeNameHandling = TypeNameHandling.Auto,
-            Formatting = Formatting.Indented,
-            ContractResolver = new CamelCasePropertyNamesContractResolver()
-        };
 
         var result = JsonConvert.SerializeObject(
-            error,
-            typeof(Models.Response),
-            jsonSerializerSettings
+            error
         );
 
-        output.WriteLine(result);
+        _output.WriteLine(result);
         Assert.Equal(ExpectedJson, result);
     }
 
     [Fact]
     public void Derived_type_deserialization() {
-        var jsonSerializerSettings = new JsonSerializerSettings {
-            TypeNameHandling = TypeNameHandling.Auto,
-            Formatting = Formatting.Indented,
-            ContractResolver = new CamelCasePropertyNamesContractResolver()
-        };
-
-        var result = JsonConvert.DeserializeObject<Response>(ExpectedJson, jsonSerializerSettings);
+        var result = JsonConvert.DeserializeObject<Response>(ExpectedJson);
 
         Assert.IsType<Error>(result);
         Assert.Equal(-1, (result as Error)?.Code);
--- BlackTitanium/Models/Response.cs
+++ BlackTitanium/Models/Response.cs
@@ -1,15 +1,23 @@
-using System.Text.Json.Serialization;
+using JsonSubTypes;
 using Newtonsoft.Json;
 
 namespace BlackTitanium.Models; 
 
-[JsonDerivedType(typeof(Error), "type")]
-public class Response {
-    // [JsonProperty("type")]
-    // public string ResponseType { get; set; }
+[JsonConverter(typeof(JsonSubtypes), "type")]
+[JsonSubtypes.KnownSubType(typeof(Error), "error")]
+[JsonSubtypes.KnownSubType(typeof(Ok), "ok")]
+public abstract class Response {
+    [JsonProperty("type")] public virtual string ResponseType { get; set; } = "none";
 }
 
 public class Error : Response {
     public string Message { get; set; } = string.Empty;
     public int Code { get; set; }
+    public override string ResponseType { get; set; } = "error";
+}
+
+public class Ok : Response {
+    public string Message => "Ok";
+    public int Code => 200;
+    public override string ResponseType { get; set; } = "ok";
 }
