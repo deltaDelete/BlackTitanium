2bed0937ea194f8ce4b7ca18e58eaf01877921a8
deltaDelete <samansy@yandex.ru>
2024-02-06 10:41:14 +0700

    feat: добавлен swagger и регистрация

--- BlackTitanium/Controllers/AuthController.cs
+++ BlackTitanium/Controllers/AuthController.cs
@@ -1,4 +1,9 @@
+using System.Globalization;
+using System.Security.Claims;
 using BlackTitanium.Models;
+using BlackTitanium.Utils;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
@@ -12,12 +17,38 @@ public class AuthController(DatabaseContext db) : ControllerBase {
     public DatabaseContext Db { get; private set; } = db;
 
     [HttpPost("register")]
-    public IActionResult Register([FromBody] RegisterRequest request) {
-        return StatusCode(503);
+    public async Task<ActionResult<Response>> Register([FromBody] RegisterRequest request) {
+        string login = request.Login;
+        string password = request.Password;
+
+        if (Db.Users.Any(u => u.Login == login)) {
+            return Conflict(new Error() {
+                Message = "User with this login already exists",
+                Code = StatusCodes.Status409Conflict
+            });
+        }
+
+        string passwordSalt = Guid.NewGuid().ToString().Split('-')[0];
+        string passwordHash = Cryptography.Sha1(password + passwordSalt);
+
+        var user = new User() {
+            Login = request.Login,
+            FirstName = request.FirstName,
+            LastName = request.LastName,
+            PasswordHash = passwordHash,
+            PasswordSalt = passwordSalt,
+            Birthday = request.Birthday,
+            GenderId = request.GenderId
+        };
+
+        var addedUser = await Db.Users.AddAsync(user);
+        await Db.SaveChangesAsync();
+
+        return Ok(addedUser.Entity);
     }
 
     [HttpPost("login")]
-    public async Task<IActionResult> Login([FromBody] LoginRequest request) {
+    public async Task<ActionResult<Models.Response>> Login([FromBody] LoginRequest request) {
         var user = await Db.Users.FirstOrDefaultAsync(x => x.Login == request.Login);
         if (user is null) {
             return BadRequest(new Error() {
@@ -25,20 +56,35 @@ public class AuthController(DatabaseContext db) : ControllerBase {
             });
         }
 
-        if (CheckPassword(user, request.Password)) {
+        if (!CheckPassword(user, request.Password)) {
             return BadRequest(new Error() {
                 Code = 401,
                 Message = "Wrong password"
             });
         }
-        
+
+        var claims = new List<Claim> {
+            new(ClaimTypes.Name, user.FirstName),
+            new(ClaimTypes.Surname, user.LastName),
+            new(ClaimTypes.Email, user.Login),
+            new("BirthDay", user.Birthday.ToString(DateTimeFormatInfo.InvariantInfo)),
+            new(ClaimTypes.NameIdentifier, user.UserId.ToString()),
+            new(ClaimTypes.Gender, user.GenderId.ToString())
+        };
+        var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+        await HttpContext.SignInAsync(
+            CookieAuthenticationDefaults.AuthenticationScheme,
+            new ClaimsPrincipal(claimsIdentity)
+        );
         return Ok(new Object<User> {
             Content = user
         });
     }
-
-    public IActionResult Index() {
-        return Ok("Hello world");
+    
+    [HttpPost("logout")]
+    public async Task<IActionResult> Logout() {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+        return Ok();
     }
 
     private static bool CheckPassword(User user, string password) {
--- BlackTitanium/Program.cs
+++ BlackTitanium/Program.cs
@@ -1,40 +1,54 @@
 using System.Text.Json;
 using BlackTitanium.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Diagnostics;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 
 namespace BlackTitanium;
 
-internal class Program {
+internal static class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
-        ConfigureServices(builder.Services);
+        builder.Services.ConfigureServices();
 
         var app = builder.Build();
         app.MapGet("/", () => "Hello World!");
 
-        ConfigureApp(app);
+        app.ConfigureApp();
 
         app.Run();
     }
 
-    public static void ConfigureServices(IServiceCollection services) {
+    public static void ConfigureServices(this IServiceCollection services) {
         services.AddDbContext<DatabaseContext>();
         services.AddControllers().AddNewtonsoftJson(x => {
             x.SerializerSettings.Formatting = Formatting.None;
             x.SerializerSettings.TypeNameHandling = TypeNameHandling.Auto;
             x.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver();
         });
+        services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
+        services.AddAuthorization();
+
+        // Swagger
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen();
     }
 
-    public static void ConfigureApp(WebApplication app) {
-// #if DEBUG
-        // app.UseDeveloperExceptionPage();
-// #endif
+    public static void ConfigureApp(this WebApplication app) {
+
+        if (app.Environment.IsDevelopment()) {
+            app.UseDeveloperExceptionPage();
+            // Swagger
+            app.UseSwagger();
+            app.UseSwaggerUI();
+        }
 
         app.UseRouting();
         app.MapControllers();
+
+        app.UseAuthentication();
+        app.UseAuthorization();
         
         // TODO LOW: Свой формат ошибок
         app.UseExceptionHandler(a => a.Run(async context =>
